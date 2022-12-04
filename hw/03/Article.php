7bc48509ab36f812286e4c4f1d6352c23f21b470
koston022 <koston022@mail.ru>
2022-12-05 00:42:25 +0500

    some added

--- hw/03/Article.php
+++ hw/03/Article.php
@@ -1,27 +1,36 @@
 <?php
-include_once "InewStorage.php";
 
+include_once 'InewStorage.php';
 class Article implements InewStorage
 {
-	public $title;
-	public $text;
+	// public $title;
+	// public $text;
 
-	public function __construct(string $title, string $text)
-	{
-		$this->title = $title;
-		$this->text = $text;
-	}
+	// public function __construct(string $title, string $text)
+	// {
+	// 	$this->title = $title;
+	// 	$this->text = $text;
+	// }
 	public function create(array $fields): int
 	{
+		// $this->title = $fields['title'];
+		// $this->text = $fields['text'];
+		$sql = "INSERT INTO article (title, text) VALUES (:title, :text)";
+		$query = dbQuery($sql, $fields);
+		var_dump($query);
+		return true;
 	}
 	public function get(int $id): ?array
 	{
+		return [];
 	}
 	public function remove(int $id): bool
 	{
+		return true;
 	}
 	public function update(int $id, array $fields): bool
 	{
+		return true;
 	}
 	protected function isValid()
 	{
--- hw/03/index.php
+++ hw/03/index.php
@@ -2,5 +2,12 @@
 
 include_once('ini.php');
 include_once('db.php');
-include_once('Article.php')
+include_once('Article.php');
 
+$article1 = new Article();
+$article1->create(['title' => 'new article', 'text' => 'some text for article']);
+
+
+echo '<pre>';
+print_r($article1);
+echo '</pre>';

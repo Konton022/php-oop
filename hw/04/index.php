aeeacaaaf0e441186df17af01ebdc9335b5aeb65
koston022 <koston022@mail.ru>
2022-12-12 23:01:49 +0500

    was added hw

--- hw/04/index.php
+++ hw/04/index.php
@@ -1,5 +1,7 @@
 <?php
 include_once 'Shop.php';
+include_once 'Istorage.php';
+
 
 use Base\Category\{Shop};
 use Base\User\{User};
@@ -15,7 +17,7 @@ trait Email
         $this->email = $email;
     }
 }
-class Car extends Shop
+class Car extends Shop implements Istorage
 {
     public $type;
     public $city;
@@ -26,7 +28,24 @@ class Car extends Shop
         $this->type = $type;
         $this->city = $city;
     }
+    function add(){
+        $data = ['id'=>$this->id, 'name'=>$this->name, 'email'=>$this->email, 'type'=>$this->type, 'city'=>$this->city];
+        $jsdata = json_encode($data);
+        file_put_contents('car.txt', $jsdata, FILE_APPEND);
+    }
+    function get(){
+        $jsdata = file_get_contents('car1.json',true);
+        return json_decode($jsdata);
+    }
+    function update(){
+
+    }
+    function delete(){
+
+    } 
+    
 }
+
 
 $carshop1 = new Car('lider', 'lider@lider', 1, 'auto', 'moscow');
 echo '<pre>';
@@ -34,14 +53,14 @@ var_dump($carshop1);
 echo '</pre>';
 // $carshop1->email = 'lada@lada';
 
-echo $carshop1->getName() . '<br>';
-echo $carshop1->getEmail() . '<br>';
+// echo $carshop1->getName() . '<br>';
+// echo $carshop1->getEmail() . '<br>';
 
 $carshop1->setEmail('wv@wv');
-echo $carshop1->getEmail() . '<br>';
+// echo $carshop1->getEmail() . '<br>';
+// $carshop1->add();
 // $product1 = new Shop('user', 'user@user.ru', 1);
-
-
+var_dump($carshop1->get());
 // var_dump($product1);
 
 // class ShoppingCard{
@@ -59,10 +78,10 @@ echo $carshop1->getEmail() . '<br>';
 // ShoppingCard::setName('Koston');
 // echo ShoppingCard::getName();
 
-$admin = new User('koston', 'koston022@mail', 'admin');
+// $admin = new User('koston', 'koston022@mail', 'admin');
 
-echo '<pre>';
-var_dump($admin);
-echo '</pre>';
+// echo '<pre>';
+// var_dump($admin);
+// echo '</pre>';
 
-echo $admin->getInfo();
+// echo $admin->getInfo();
